72baf71091a9a49500140231066d979ba651a64f
cephscope <sworm.null@gmail.com>
2018-09-14 20:14:42 -0400

    refactored Main to stateless component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import Konva from 'konva';
 import { Stage, Layer } from 'react-konva';
 import Controls from './Controls';
 import LeadPlayer from './LeadPlayer';
 import { P5Wrapper } from './P5Wrapper';
 import sketch from '../utils/sketch.js';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="header">calm corner.</h1>
-        <a href="https://github.com/cephscope/noise" className="header">github</a>
-        <div className="content-container">
-          <Controls />
-          <Stage container="app" height={100} width={850}>
-            <Layer>
-              <LeadPlayer />
-            </Layer>
-          </Stage>
-        </div>
-        <P5Wrapper sketch={sketch} />
+const Main = () => {
+  return (
+    <div>
+      <h1 className="header">calm corner.</h1>
+      <a href="https://github.com/cephscope/noise" className="header">github</a>
+      <div className="content-container">
+        <Controls />
+        <Stage container="app" height={100} width={850}>
+          <Layer>
+            <LeadPlayer />
+          </Layer>
+        </Stage>
       </div>
-    );
-  };
+      <P5Wrapper sketch={sketch} />
+    </div>
+  );
 }
 
 export default Main;
